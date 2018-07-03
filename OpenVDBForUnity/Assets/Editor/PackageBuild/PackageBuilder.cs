41ca17d94c59efc8cc29572f84cbba25738a965d
kazuki matsumoto <karasusan@gmail.com>
2018-07-03 17:11:51 +0900

    Add UnityPackage build script

---
+++ OpenVDBForUnity/Assets/Editor/PackageBuild/PackageBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Assets.Editor.PackageBuild
+{
+    class PackageBuildInfo
+    {
+        public bool succeed;
+        public string exportPath;
+        public long fileSize;
+    }
+
+    static class PackageBuilder
+    {
+        const string PackageFileName = "OpenVDB.unitypackage";
+        static string[] AssetPathNames =
+        {
+            "Assets/OpenVDB"
+        };
+
+        public static PackageBuildInfo Build(string exportDir)
+        {
+            var flags = ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets;
+            var exportPath = Path.Combine(exportDir, PackageFileName);
+            AssetDatabase.ExportPackage(AssetPathNames, exportPath, flags);
+
+            var succeed = File.Exists(exportPath);
+            long fileSize = 0;
+
+            if(succeed)
+            {
+                var info = new FileInfo(exportPath);
+                fileSize = info.Length;
+            }
+
+            return new PackageBuildInfo
+            {
+                succeed = succeed,
+                exportPath = exportPath,
+                fileSize = fileSize
+            };
+        }
+    }
+}
---
+++ OpenVDBForUnity/Assets/Editor/PackageBuild/UnityCloudBuildTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Editor.PackageBuild
+{
+    static class UnityCloudBuildTrigger
+    {
+        public static void PostExport(string exportPath)
+        {
+            Debug.LogFormat("PostExport Start");
+            var info = PackageBuilder.Build(exportPath);
+
+            Debug.LogFormat("PostExport End. Build {0}. FileSize:{1} ExportPath:{2}", 
+                            info.succeed ? "Succeed" : "Failed", 
+                            info.fileSize, 
+                            info.exportPath);
+        }
+    }
+}
