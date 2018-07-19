0683099a06a7da118a448e6d08609bfaa2497874
kazuki matsumoto <karasusan@gmail.com>
2018-07-19 18:02:16 +0900

    Create ShaderGraph Node

--- OpenVDBForUnity/Assets/OpenVDB/Editor/ShaderGraph/Data/Nodes/Volume/DensityRayMarchNode.cs
+++ OpenVDBForUnity/Assets/OpenVDB/Editor/ShaderGraph/Data/Nodes/Volume/DensityRayMarchNode.cs
@@ -24,26 +24,41 @@ namespace OpenVDB
 
         static string DensityRayMarch(
             [Slot(1, Binding.None)] Texture3D Tex,
-            [Slot(2, Binding.None)] Vector1 MaxSteps,
-            [Slot(3, Binding.None)] Vector3 CamDir,
-            [Slot(4, Binding.None)] Vector3 LocalPos,
-            [Slot(5, Binding.None)] out Vector1 Out)
+            [Slot(2, Binding.None)] SamplerState TexSamplerState,
+            [Slot(3, Binding.None)] Vector1 MaxSteps,
+            [Slot(4, Binding.None)] Vector3 WorldPos,
+            [Slot(5, Binding.None)] Vector1 Density,
+            [Slot(6, Binding.None)] Vector3 UVW,
+            [Slot(7, Binding.None)] out Vector4 Out)
         {
+            Out = Vector4.zero;
             return 
                 @"
 {
     float accumdist = 0;
-    //float3 localcamvec = normalize( mul(CamDir, (float3x3)unity_WorldToObject) );
-    float3 localcamvec = normalize( CamDir );
+    float curdensity = 0;
+    float transmittance = 1;
+
+    float3 dir = normalize(WorldPos - _WorldSpaceCameraPos);
+    float3 localcamvec = normalize(mul((float3x3) unity_WorldToObject, dir));
+    float3 localPos = mul(unity_WorldToObject, float4(WorldPos, 1)).xyz;
     float StepSize = 1.0 / MaxSteps;
+
+    Density *= StepSize;
 
     for (int i = 0; i < MaxSteps; i++)
     {
-        float cursample = 0.0;
+        float cursample = SAMPLE_TEXTURE3D(Tex, TexSamplerState, saturate(localPos)).r;
         accumdist += cursample * StepSize;
-        LocalPos += -localcamvec * StepSize;
+        localPos += -localcamvec * StepSize;
+
+        if(cursample > 0.001)
+        {
+            curdensity = saturate(cursample * Density);
+            transmittance *= 1-curdensity;
+        }
     }
-    Out = accumdist;
+    Out = float4( 1, 1, 1, transmittance);
 }";
         }
     }
