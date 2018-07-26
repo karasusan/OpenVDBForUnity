6cd0dd625395d70c82656c7eb26f0472aaf41107
kazuki matsumoto <karasusan@gmail.com>
2018-07-26 15:48:50 +0900

    Fix UnitTest Error

--- OpenVDBForUnity/Assets/OpenVDB.Tests/Const.cs
+++ OpenVDBForUnity/Assets/OpenVDB.Tests/Const.cs
@@ -2,7 +2,7 @@
 {
     public static class Const
     {
-        public const string VDBSampleFilePath = "Packages/jp.kazuki.openvdb-samplemodels/OpenVDBSampleModels/bunny.vdb";
+        public const string VDBSampleFilePath = "Packages/jp.kazuki.openvdb-samplemodels/OpenVDBSampleModels/bunny_cloud.vdb";
 
         public const string AssetTempPath = "Assets/temp";
 
